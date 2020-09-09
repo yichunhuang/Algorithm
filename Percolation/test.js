cb9d0c859483d8264a71ae3d5f88ce561babd159
Yi Chun <b03704074@ntu.edu.tw>
2020-09-09 15:38:58 +0800

    add one unit test

--- Percolation/test.js
+++ Percolation/test.js
@@ -35,5 +35,37 @@ describe('Percolation', function () {
 			})
 			const result = percolation.percolates() ? true : false
 			assert.equal(result, false)
+		}),
+		it('input 6 - true', function () {
+			const data = {
+				size: 6,
+				open: [
+					[1, 6],
+
+					[2, 6],
+					[3, 6],
+					[4, 6],
+					[5, 6],
+					[5, 5],
+					[4, 4],
+					[3, 4],
+					[2, 4],
+					[2, 3],
+					[2, 2],
+					[2, 1],
+					[3, 1],
+					[4, 1],
+					[5, 1],
+					[5, 2],
+					[6, 2],
+					[5, 4]
+				]
+			}
+			const percolation = new Percolation(data.size)
+			data.open.forEach(open => {
+				percolation.open(open[0], open[1])
+			})
+			const result = percolation.percolates() ? true : false
+			assert.equal(result, true)
 		})
 })
